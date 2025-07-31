a1ed5504db9864fe11ec0b8b64ca68a315633f70
Pronoy Das <pronoysunny.47@gmail.com>
2025-07-31 10:18:51 +0530

    Fix selenium Grid Host

--- src/test/java/com/pronoydas/tests/BaseTest.java
+++ src/test/java/com/pronoydas/tests/BaseTest.java
@@ -1,6 +1,7 @@
 package com.pronoydas.tests;
 
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 
 import org.openqa.selenium.Capabilities;
@@ -10,6 +11,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.devtools.v135.filesystem.model.BucketFileSystemLocator;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.slf4j.LoggerFactory;
 import org.testng.ITest;
 import org.testng.ITestContext;
 import org.testng.ITestNGListener;
@@ -18,17 +20,22 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import com.pronoydas.utility.Constants;
 import com.pronoydas.utility.PropertyLoader;
 
+import ch.qos.logback.classic.Logger;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
 	
 	protected WebDriver driver ;
+	private static final Logger log = (Logger) LoggerFactory.getLogger(BaseTest.class);
 	
 	@BeforeSuite
 	public void loadConfig() {
+		log.info("Executing LoadConfig");
 		PropertyLoader.initialize();
+		log.info("Comptete Executing LoadConfig");
 	}
 	
 	
@@ -36,36 +43,50 @@ public class BaseTest {
 	@BeforeMethod
 	public void Steup(ITestContext context) throws Exception {
 		
-		                                               
+		log.info("Start Setup!!");                                               
 		String boolVal=PropertyLoader.getPropertyValue("selenium.grid.enabled");
 		if(Boolean.parseBoolean(boolVal)) {
+			log.info("Getting Remote Browser Info"); 
 			driver = getRemoteBrowser();
+			log.info("Init WebDriver"); 
 		}else {
 			driver = getLocalBrowser();
 		}
 		driver.manage().window().maximize();
 		context.setAttribute("DRIVER", this.driver);
+		log.info("Looking for Test Suite");
 		if(context.getName().equals("FlightReservation_E2E")) {
 			String appUrl=PropertyLoader.getPropertyValue("flightReservationUrl");
 		  driver.get(appUrl);
+		  log.info("Start Executing flightReservation Tests");
 			
 		}else {
 			String appUrl=PropertyLoader.getPropertyValue("vendorapplicationUrl");
 			driver.get(appUrl);
+			log.info("Start Executing vendorapplication Tests");
 			
 		}
 		
 	}
 	private WebDriver getRemoteBrowser() throws Exception {
+		String urlFormat = PropertyLoader.getPropertyValue(Constants.GRID_URL_FORMAT);
+        String hubHost = PropertyLoader.getPropertyValue(Constants.GRID_HUB_HOST);
+        String url = String.format(urlFormat, hubHost);
+        log.info("grid url: {}", url);
 		Capabilities cap ;
+
 		String browser=PropertyLoader.getPropertyValue("browserName");
+		log.info("Reading Browser Name "+browser);
 		if(browser.equalsIgnoreCase(browser)) {
 			cap = new ChromeOptions();
 		}
 		else {
 			cap = new FirefoxOptions();
 		}
-		return new RemoteWebDriver(new URL(PropertyLoader.getPropertyValue("selenium.grid.urlFormat")) , cap);
+		log.info("Start Creating  Object Of Remote Web Driver!! ");
+		RemoteWebDriver driver= new RemoteWebDriver(new URI(url).toURL() , cap);
+		log.info("Created Object Of Remote Web Driver!! ");
+		return driver;
 	}
 	
 	private WebDriver getLocalBrowser() {
